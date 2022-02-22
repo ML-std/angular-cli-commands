b6c4d50ae9f757ca71c0ffcab8b203803f5f54f5
ml-std <m.barisege@gmail.com>
2022-02-22 12:12:15 +0300

    command line operations -1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { AuthGuard } from './auth.guard';
+import { ContactListComponent } from './contact-list/contact-list.component';
+const routes: Routes = [{
+  path:'contacts',
+  component:ContactListComponent,
+  canActivate: [AuthGuard]
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
